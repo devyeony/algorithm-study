a7ff6d1ef51932524a4eada051d9e2ef288b2c82
Hayden <devyeony@gmail.com>
2022-06-02 00:03:56 +0900

    #2 - LeetCode : 2160. Minimum Sum of Four Digit Number After Splitting Digits

--- 01_math/Hayden/LeetCode/2160_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py
+++ 01_math/Hayden/LeetCode/2160_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.py
@@ -6,4 +6,15 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        
+        first = num // 1000
+        second = num % 1000 // 100
+        third = num % 100 // 10
+        fourth = num % 10
+        nums = [first, second, third, fourth]
+        nums.sort()
+        return nums[0] * 10 + nums[1] * 10 + nums[2] + nums[3]
+
+
+sol = Solution()
+print(sol.minimumSum(2932))
+print(sol.minimumSum(4009))
