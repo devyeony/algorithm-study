eb6fd1e0fb2b114734655bf773d2cde086a0a34f
Hayden <devyeony@gmail.com>
2022-06-13 23:45:01 +0900

    #13 - LeetCode : 1859. Sorting the Sentence

---
+++ 02_string/Hayden/LeetCode/1859_Sorting_the_Sentence.py
@@ -0,0 +1,9 @@
+# #13 - LeetCode : 1859. Sorting the Sentence
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+
+sol = Solution()
+print(sol.toLowerCase("Hello"))
