7b0d1e5493b63a87f38fdd103248f922a8d12184
Yeonhee Kim <devyeony@gmail.com>
2022-06-15 08:41:19 +0900

    #13 - LeetCode : 1769. Minimum Number of Operations to Move All Balls to Each Box

--- 02_string/Hayden/LeetCode/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.py
+++ 02_string/Hayden/LeetCode/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.py
@@ -2,7 +2,7 @@
 from typing import List
 
 
-# 1차 시도 : Time Limit Exceeded
+# 1차 시도 : 8356 ms
 class Solution1:
     def minOperations(self, boxes: str) -> List[int]:
         count_list: List[int] = [0] * len(boxes)
@@ -15,10 +15,20 @@ class Solution1:
         return count_list
 
 
-# 2차 시도 : ing
+# 2차 시도 : 92 ms
 class Solution2:
     def minOperations(self, boxes: str) -> List[int]:
-        count_list: List[int] = [0] * len(boxes)
+        count_list = [0] * len(boxes)
+        ones = 0  # 1의 개수
+        for i, box in enumerate(boxes):
+            if box == '1':
+                count_list[0] += i
+                ones += 1
+        visited = 0
+        for i in range(1, len(boxes)):
+            count_list[i] = count_list[i-1] - (ones - visited) + visited
+            if boxes[i] == '1':
+                visited += 1
         return count_list
 
 
