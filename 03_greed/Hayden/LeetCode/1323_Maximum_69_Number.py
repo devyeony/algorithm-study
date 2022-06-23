b5589c477162e472b04083753e0b38dbf52cce32
Yeonhee Kim <devyeony@gmail.com>
2022-06-23 09:41:29 +0900

    #21 - LeetCode : 1323. Maximum 69 Number

--- 03_greed/Hayden/LeetCode/1323_Maximum_69_Number.py
+++ 03_greed/Hayden/LeetCode/1323_Maximum_69_Number.py
@@ -1,14 +1,16 @@
 # #21 - LeetCode : 1323. Maximum 69 Number
+from typing import List
+
 
 class Solution:
     def maximum69Number(self, num: int) -> int:
-        num_str = str(num)
-        index = 0
+        num_str: str = str(num)
+        index: int = 0
         for idx, val in enumerate(num_str):
             if val != '9':
                 index = idx
                 break
-        num_list = list(num_str)
+        num_list: List[str] = list(num_str)
         num_list[index] = '9'
         return ''.join(num_list)
 
