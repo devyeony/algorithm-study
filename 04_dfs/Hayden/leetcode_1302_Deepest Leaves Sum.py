9862c494c0f24542e28b0bae36d8c6956ec728ef
Hayden <devyeony@gmail.com>
2022-07-14 17:18:04 +0900

    Create leetcode_1302_Deepest Leaves Sum.py

---
+++ 04_dfs/Hayden/leetcode_1302_Deepest Leaves Sum.py
@@ -0,0 +1,4 @@
+# #30 - LeetCode : 1302. Deepest Leaves Sum
+
+
+# TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(7, None, None), None), TreeNode(5, None, None)), TreeNode(3, None, TreeNode(6, None, TreeNode(8, None, None))))
