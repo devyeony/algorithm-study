162558d15aadf1df91a57903ead919102354a094
Hayden <devyeony@gmail.com>
2022-07-14 17:32:48 +0900

    Update leetcode_1302_Deepest Leaves Sum.py

--- 04_dfs/Hayden/leetcode_1302_Deepest Leaves Sum.py
+++ 04_dfs/Hayden/leetcode_1302_Deepest Leaves Sum.py
@@ -1,4 +1,31 @@
 # #30 - LeetCode : 1302. Deepest Leaves Sum
 
+from typing import Optional
 
-# TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(7, None, None), None), TreeNode(5, None, None)), TreeNode(3, None, TreeNode(6, None, TreeNode(8, None, None))))
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        return
+
+
+# input1
+leaf1: Optional[TreeNode] = TreeNode(7, None, None)
+parent1: Optional[TreeNode] = TreeNode(4, leaf1, None)
+parent2: Optional[TreeNode] = TreeNode(5, None, None)
+grand_parent1: Optional[TreeNode] = TreeNode(2, parent1, parent2)
+
+leaf2: Optional[TreeNode] = TreeNode(8, None, None)
+parent3: Optional[TreeNode] = TreeNode(6, None, leaf2)
+grand_parent2: Optional[TreeNode] = TreeNode(3, None, parent3)
+
+root1: Optional[TreeNode] = TreeNode(1, grand_parent1, grand_parent2)
+
+# run1
+sol = Solution()
+print(sol.deepestLeavesSum(root1))
+
