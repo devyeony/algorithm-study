39aa18ae4648c6d2a9bd8965dd9b91f500f1e6ea
Hayden <devyeony@gmail.com>
2022-07-14 17:14:01 +0900

    # #30 - LeetCode : 1022. Sum of Root To Leaf Binary Numbers

---
+++ 04_dfs/Hayden/leetcode_1022_Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,45 @@
+# #30 - LeetCode : 1022. Sum of Root To Leaf Binary Numbers
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        self.sum = 0
+        self.dfs(root, str(root.val))
+        return self.sum
+
+    def dfs(self, node: Optional[TreeNode], current_str: str):
+        print("current_str : ", current_str)
+        if not node.left and not node.right:
+            self.sum += int(current_str, 2)
+            print("========================")
+            return
+
+        if node.left:
+            self.dfs(node.left, current_str + str(node.left.val))
+        if node.right:
+            self.dfs(node.right, current_str + str(node.right.val))
+
+
+leaf1: Optional[TreeNode] = TreeNode(0, None, None)
+leaf2: Optional[TreeNode] = TreeNode(1, None, None)
+middle1: Optional[TreeNode] = TreeNode(0, leaf1, leaf2)
+
+leaf3: Optional[TreeNode] = TreeNode(0, None, None)
+leaf4: Optional[TreeNode] = TreeNode(1, None, None)
+middle2: Optional[TreeNode] = TreeNode(1, leaf3, leaf4)
+
+root: Optional[TreeNode] = TreeNode(1, middle1, middle2)
+
+sol = Solution()
+print(sol.sumRootToLeaf(root))
