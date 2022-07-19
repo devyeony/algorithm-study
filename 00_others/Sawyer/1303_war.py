e0ee5e27b044f16922dd32ec95d9956062ef527a
jo1013 <tkdah0@gmail.com>
2022-07-20 00:18:01 +0900

    #30

--- 00_others/Sawyer/1303_war.py
+++ 00_others/Sawyer/1303_war.py
@@ -1,34 +1,52 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+n, m = map(int, input().split())
+graph = [list(input().strip()) for _ in range(m)]
+white, blue = 0, 0
 
 
-def bfs(graph, start):
-    num = [0] * (n+1) # 그래프와의 거리를 넣을 리스트
-    visited = [start] # 방문여부
-    queue = deque()  # popleft를 위해서 deque사용
-    queue.append(start) # 큐에 출발위치 넣어주기
+def dfs(x, y, std, count):   
+    graph[x][y] = 'visited'
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < m and 0 <= ny < n:
+            if graph[nx][ny] != 'visited' and graph[nx][ny] == std:
+                count = dfs(nx, ny, std, count + 1)
+    return count
 
-    while queue: # 큐에 값이 있으면 계속돌아주기
-        a = queue.popleft() # 큐에 먼저들어온값 빼주기
-        for i in graph[a]: # 그래프리스트의 [a]가 value이기때문에 확인
-            if i not in visited: # 방문안한 곳이면 방문 
-                num[i] = num[a] + 1 # 종목이 기존 경로에 있으면 기존거리 + 1
-                visited.append(i) # 방문리스트에 추가
-                queue.append(i) # 큐에 넣어주기
-    return sum(num)
+def bfs(i, j, std):
+    Q = deque()
+    Q.append((i, j))
+    graph[i][j] = 'visited'
+    count = 0
+    
+    while Q:
+        x, y = Q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if graph[nx][ny] != 'visited' and graph[nx][ny] == std:
+                    graph[nx][ny] = 'visited'
+                    Q.append((nx, ny))
+                    count += 1
+    return count + 1
 
 
-if __name__ == '__main__':
-    n, m = map(int, sys.stdin.readline().split())
-    # graph = [[] for _ in range(n+1)] 
-    # for i in range(m):
-    #     a, b = map(int, sys.stdin.readline().split())
-    #     graph[a].append(b)
-    #     graph[b].append(a)
-
-    # result = []
-    # for i in range(1, n+1):
-    #     result.append(bfs(graph, i))
-    temp = [] 
-    graph = [temp + [] input()]
-    print(result.index(min(result))+1)
+for i in range(m):
+    for j in range(n):
+        if graph[i][j] != 'visited':
+            if graph[i][j] == 'W':
+                white += bfs(i, j, 'W')**2
+                # dfs : 
+                # white += dfs(i, j, 'W', 1)**2
+            else:
+                blue += bfs(i, j, 'B')**2
+                # dfs: 
+                # blue += dfs(i, j, 'B', 1)**2
+ 
+print(white, blue)
